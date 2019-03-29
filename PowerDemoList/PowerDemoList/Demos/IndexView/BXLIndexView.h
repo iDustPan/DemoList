f243927aca96771406b22af75666a97e9c84a442
borderxlab_pan <pan@borderxlab.com>
2019-03-29 13:26:00 +0800

    add selection indicator.

--- PowerDemoList/PowerDemoList/Demos/IndexView/BXLIndexView.h
+++ PowerDemoList/PowerDemoList/Demos/IndexView/BXLIndexView.h
@@ -19,11 +19,17 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) UIColor *textColor;
 @property (nonatomic, strong) UIColor *highlightTextColor;
 
+@property (nonatomic, strong) UIColor *highlightBgColor;
+
 @property (nonatomic, assign) CGFloat spacing;
 
 @property (nonatomic, assign) CGFloat fontSize;
 
 @property (nonatomic, assign) CGFloat itemWidth;
+
+@property (nonatomic, assign) CGFloat indicatorRightMargin;
+
+@property (nonatomic, assign) CGFloat indicatorWidth;
 
 
 @end
--- PowerDemoList/PowerDemoList/Demos/IndexView/BXLIndexView.m
+++ PowerDemoList/PowerDemoList/Demos/IndexView/BXLIndexView.m
@@ -13,46 +13,84 @@
 
 @property (nonatomic, strong) CAIndexItemLayer *itemLayer;
 
+@property (nonatomic, assign) NSInteger selectedSection;
+
+@property (nonatomic, strong) UILabel *indicator;
+
+@property (nonatomic, strong) NSMutableArray<CALayer *> *subTextLayer;
+
+@property (nonatomic, strong) UIImpactFeedbackGenerator *generator NS_AVAILABLE_IOS(10_0);
+
 @end
 
 @implementation BXLIndexView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
+        _subTextLayer = @[].mutableCopy;
         _spacing = 2;
         _fontSize = 12;
         _itemWidth = 16;
+        _textColor = [UIColor blackColor];
+        _highlightBgColor = [UIColor blackColor];
+        _highlightTextColor = [UIColor whiteColor];
+        _indicatorRightMargin = 20;
+        _indicatorWidth = 40;
+        [self addSubview:self.indicator];
     }
     return self;
 }
 
-- (CALayer *)layerAtPoint:(CGPoint)point {
-    CALayer *touchedLayer = nil;
-    for (CALayer *layer in self.layer.sublayers) {
-        if ([layer containsPoint:point]) {
-            return layer;
-        }
+- (void)setSelectedSection:(NSInteger)selectedSection {
+    if (selectedSection != _selectedSection) {
+        [self configureSublayers:NO];
+        _selectedSection = selectedSection;
+        [self configureSublayers:YES];
+        [self touchedFeedBack];
     }
-    return touchedLayer;
+}
+
+- (void)touchedFeedBack {
+    if (@available(iOS 10.0, *)) {
+        [self.generator prepare];
+        [self.generator impactOccurred];
+    }
+}
+
+- (void)configureSublayers:(BOOL)selected {
+    if (self.selectedSection != 0) {
+        CATextLayer *touchedLayer = (CATextLayer *)self.subTextLayer[self.selectedSection];
+        [CATransaction begin];
+        [CATransaction setDisableActions:YES];
+        if (selected) {
+            touchedLayer.backgroundColor = _highlightBgColor.CGColor;
+            touchedLayer.foregroundColor = _highlightTextColor.CGColor;
+        }else{
+            touchedLayer.backgroundColor = self.backgroundColor.CGColor;
+            touchedLayer.foregroundColor = _textColor.CGColor;
+        }
+        [self layoutIndicator:touchedLayer];
+        [CATransaction commit];
+    }
 }
 
 - (void)configureTouchedLayerAtPoint:(CGPoint)point {
-    for (CALayer *layer in self.layer.sublayers) {
-        CGPoint coverPoint = [self.layer convertPoint:point toLayer:layer];
-        [CATransaction begin];
-        [CATransaction setDisableActions:YES];
-        if ([layer containsPoint:coverPoint]) {
-            layer.backgroundColor = [UIColor blackColor].CGColor;
-        }else{
-            layer.backgroundColor = self.backgroundColor.CGColor;
-        }
-        [CATransaction commit];
+    NSInteger section = (point.y + _spacing)/(_itemWidth + _spacing);
+    if (point.y < 0) {
+        section = 0;
+        [self showIndicator:NO];
+    }else if (section >= _indexes.count) {
+        section = _indexes.count - 1;
+    }else{
+        [self showIndicator:YES];
     }
+    self.selectedSection = section;
 }
 
 - (BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
     CGPoint startPoint = [touch locationInView:self];
     [self configureTouchedLayerAtPoint:startPoint];
+    [self showIndicator:YES];
     return [super beginTrackingWithTouch:touch withEvent:event];
 }
 
@@ -62,8 +100,17 @@
     return [super continueTrackingWithTouch:touch withEvent:event];
 }
 
+- (void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self showIndicator:NO];
+}
+
+- (void)cancelTrackingWithEvent:(UIEvent *)event {
+    [self showIndicator:NO];
+}
+
 - (void)setIndexes:(NSArray<NSString *> *)indexes {
     _indexes = indexes;
+    [self.subTextLayer makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
     [self addIndexItems];
 }
 
@@ -78,13 +125,34 @@
             layer = [self searchLayer];
         }
         layer.frame = CGRectMake((self.frame.size.width - _itemWidth) * .5f, positionY, _itemWidth, _itemWidth);
+        layer.cornerRadius = _itemWidth * .5f;
         [self.layer addSublayer:layer];
         [self.layer setNeedsDisplay];
         positionY += _itemWidth + _spacing;
+        [_subTextLayer addObject:layer];
     }
 }
 
-- (CALayer *)searchLayer {
+- (void)showIndicator:(BOOL)show {
+    if (show) {
+        self.indicator.alpha = 1;
+    }else{
+        [UIView animateWithDuration:.35f delay:0 options:0 animations:^{
+            self.indicator.alpha--;
+        } completion:nil];
+    }
+}
+
+- (void)layoutIndicator:(CATextLayer *)layer {
+    self.indicator.center = CGPointMake(-(_indicatorRightMargin + _indicatorWidth/2),
+                                        layer.frame.origin.y + layer.frame.size.height * .5f);
+    self.indicator.bounds = CGRectMake(0, 0, _indicatorWidth, _indicatorWidth);
+    self.indicator.text = layer.string;
+    self.indicator.layer.cornerRadius = self.indicator.bounds.size.width * .5f;
+    self.indicator.clipsToBounds = YES;
+}
+
+- (CAShapeLayer *)searchLayer {
     CAShapeLayer *layer = [CAShapeLayer layer];
     CGFloat radius = 4;
     CGFloat end = (_itemWidth / 2 + radius * sin(M_PI_4));
@@ -95,7 +163,7 @@
     [path addArcWithCenter:CGPointMake(_itemWidth / 2, _itemWidth / 2)
                     radius:radius
                 startAngle:M_PI_4
-                  endAngle:(M_PI_4 + M_PI*2)
+                  endAngle:(2.25*M_PI)
                  clockwise:true];
     path.lineWidth = 1;
     layer.path = path.CGPath;
@@ -110,7 +178,25 @@
     txtLayer.fontSize = _fontSize;
     txtLayer.alignmentMode = kCAAlignmentCenter;
     txtLayer.foregroundColor = [UIColor blackColor].CGColor;
+    txtLayer.contentsScale = [UIScreen mainScreen].scale;
     return txtLayer;
 }
+
+- (UILabel *)indicator {
+    if (!_indicator) {
+        _indicator = [[UILabel alloc] initWithFrame:CGRectZero];
+        _indicator.backgroundColor = [UIColor lightGrayColor];
+        _indicator.textAlignment = NSTextAlignmentCenter;
+    }
+    return _indicator;
+}
+
+- (UIImpactFeedbackGenerator *)generator {
+    if (!_generator) {
+        _generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
+    }
+    return _generator;
+}
+
 
 @end
